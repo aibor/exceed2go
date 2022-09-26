745070b6846447b62cc95f36fb54bf7d7d5effba
Tobias Böhm <code@aibor.de>
2022-09-26 20:28:15 +0200

    Use bpf iterator func, formating, doc

--- bpf/exceed2go.c
+++ bpf/exceed2go.c
@@ -18,15 +18,19 @@
 #define ADD_HDR_LEN         (sizeof(struct ipv6hdr) + sizeof(struct icmp6hdr))
 
 #define bpf_memcpy __builtin_memcpy
-#define IN6_ARE_ADDR_EQUAL(a, b)                                               \
-  ((((const __u32 *)(a))[0] == ((const __u32 *)(b))[0]) &&                     \
-   (((const __u32 *)(a))[1] == ((const __u32 *)(b))[1]) &&                     \
-   (((const __u32 *)(a))[2] == ((const __u32 *)(b))[2]) &&                     \
+#define IN6_ARE_ADDR_EQUAL(a, b) \
+  ((((const __u32 *)(a))[0] == ((const __u32 *)(b))[0]) && \
+   (((const __u32 *)(a))[1] == ((const __u32 *)(b))[1]) && \
+   (((const __u32 *)(a))[2] == ((const __u32 *)(b))[2]) && \
    (((const __u32 *)(a))[3] == ((const __u32 *)(b))[3]))
-#define NEXT_HDR(h) ((void *)(h + 1))
-#define CHECK_BOUNDARY(h, end, ret)                                            \
-  if (NEXT_HDR(h) > end) {                                                     \
-    return ret;                                                                \
+#define next_header(h) ((void *)(h + 1))
+#define assert_boundary(h, end, ret) \
+  if (next_header(h) > end) { \
+    return ret; \
+  }
+#define assert_equal(f, e, ret) \
+  if (f != e) { \
+    return ret; \
   }
 
 struct {
@@ -47,7 +51,7 @@ enum exceed_counter_key {
   COUNTER_KEY_IPV6,
   COUNTER_KEY_TARGET,
   COUNTER_KEY_FOUND,
-  COUNTER_KEY_DROP,
+  COUNTER_KEY_ABORTED,
   COUNTER_KEY_ICMP,
   COUNTER_KEY_ICMP_ECHO_REQ,
 };
@@ -59,14 +63,51 @@ static __always_inline void counter_increment(__u32 key) {
   }
 }
 
+struct target_search_cb_ctx {
+  struct in6_addr *needle;
+  __u32            key;
+  bool             found;
+};
+
+/* Callback function for bpf_for_each_map_elem for iterating over exceed_addrs
+ * map looking up the given needle by the callback context struct.
+ */
+static long target_search_cb(void                        *map,
+                             __u32                       *key,
+                             struct in6_addr             *value,
+                             struct target_search_cb_ctx *ctx) {
+  /* If lookup returns nothing or found address is zero exit as that indicates
+   *we are through the list of configured addresses.
+   */
+  if (!value || !((const __u32 *)(value))[0])
+    return 1;
+
+  if (IN6_ARE_ADDR_EQUAL(ctx->needle, value)) {
+    /* Found the destination address in our list. Exit from the iteration. */
+    ctx->found = true;
+    ctx->key   = *key;
+
+    return 1;
+  }
+
+  return 0;
+}
+
+/* Fold a 32bit integer checksum down to 16bit value as needed in protocol
+ * headers.
+ */
 static __always_inline __u16 csum_fold(__u32 sum) {
   sum = (sum & 0xffff) + (sum >> 16);
   sum = (sum & 0xffff) + (sum >> 16);
   return (__u16)~sum;
 }
 
+/* Calculate ICMPv6 header checksum. This sums up the IPv6 pseudo header of the
+ * given ipv6hdr struct, the ICMPv6 header and the payload.
+ */
 static __always_inline __u32 icmp6_csum(struct icmp6hdr *icmp6,
-                                        struct ipv6hdr *ipv6, void *data_end) {
+                                        struct ipv6hdr  *ipv6,
+                                        void            *data_end) {
   /* Value is already in network byte order. */
   __be32 len     = ((__u32)ipv6->payload_len) << 16;
   __be32 nexthdr = ((__u32)ipv6->nexthdr) << 24;
@@ -103,6 +144,9 @@ static __always_inline __u32 icmp6_csum(struct icmp6hdr *icmp6,
   return sum;
 }
 
+/* Create a ICMPv6 time exceeded message for the current packet and send it
+ * from the given src_addr.
+ */
 static __always_inline int reply_exceeded(struct xdp_md   *ctx,
                                           struct in6_addr *src_addr) {
   /* The ICMP time exceeded packet may not be longer than IPv6 minimum MTU.
@@ -110,8 +154,8 @@ static __always_inline int reply_exceeded(struct xdp_md   *ctx,
    */
   int tail_adjust = IPV6_MTU_MIN - ADD_HDR_LEN - (ctx->data_end - ctx->data);
 
-  if (bpf_xdp_adjust_head(ctx, (0 - (int)ADD_HDR_LEN)) != 0)
-    return XDP_DROP;
+  assert_equal(
+      bpf_xdp_adjust_head(ctx, (0 - (int)ADD_HDR_LEN)), 0, XDP_ABORTED);
 
   if (tail_adjust < 0)
     bpf_xdp_adjust_tail(ctx, tail_adjust);
@@ -122,12 +166,12 @@ static __always_inline int reply_exceeded(struct xdp_md   *ctx,
 
   /* Initialize former header positions. */
   struct ethhdr  *orig_eth  = (void *)data + (int)ADD_HDR_LEN;
-  struct ipv6hdr *orig_ipv6 = NEXT_HDR(orig_eth);
-  CHECK_BOUNDARY(orig_ipv6, data_end, XDP_DROP);
+  struct ipv6hdr *orig_ipv6 = next_header(orig_eth);
+  assert_boundary(orig_ipv6, data_end, XDP_ABORTED);
 
   /* Initialize new headers. */
   struct ethhdr  *eth  = (void *)data;
-  struct ipv6hdr *ipv6 = NEXT_HDR(eth);
+  struct ipv6hdr *ipv6 = next_header(eth);
 
   /* Relocate data and swap mac addresses. */
   bpf_memcpy(eth->h_source, orig_eth->h_dest, ETH_ALEN);
@@ -135,7 +179,7 @@ static __always_inline int reply_exceeded(struct xdp_md   *ctx,
   eth->h_proto = orig_eth->h_proto;
 
   /* Create new ICMP6 layer. */
-  struct icmp6hdr *icmp6 = NEXT_HDR(ipv6);
+  struct icmp6hdr *icmp6 = next_header(ipv6);
 
   __u16 payload_len = data_end - (void *)icmp6;
 
@@ -162,8 +206,11 @@ static __always_inline int reply_exceeded(struct xdp_md   *ctx,
   return XDP_TX;
 }
 
-static __always_inline int reply_echo(struct ethhdr *eth, struct ipv6hdr *ipv6,
-                                      struct icmp6hdr *icmp6, void *data_end) {
+/* Create ICMPv6 echo reply message for the given packet. */
+static __always_inline int reply_echo(struct ethhdr   *eth,
+                                      struct ipv6hdr  *ipv6,
+                                      struct icmp6hdr *icmp6,
+                                      void            *data_end) {
 
   /* Swap ethernet addresses. */
   char tmphwaddr[ETH_ALEN];
@@ -193,41 +240,22 @@ int exceed2go(struct xdp_md *ctx) {
   void *data_end = (void *)(unsigned long)ctx->data_end;
 
   struct ethhdr *eth = data;
-  CHECK_BOUNDARY(eth, data_end, DEFAULT_ACTION);
-
-  if (eth->h_proto != bpf_htons(ETH_P_IPV6))
-    return DEFAULT_ACTION;
-
+  assert_boundary(eth, data_end, DEFAULT_ACTION);
+  assert_equal(eth->h_proto, bpf_htons(ETH_P_IPV6), DEFAULT_ACTION);
   counter_increment(COUNTER_KEY_IPV6);
 
-  struct ipv6hdr *ipv6 = NEXT_HDR(eth);
-  CHECK_BOUNDARY(ipv6, data_end, DEFAULT_ACTION);
+  struct ipv6hdr *ipv6 = next_header(eth);
+  assert_boundary(ipv6, data_end, DEFAULT_ACTION);
 
-  struct in6_addr *target = NULL;
-  __u32            target_key;
-  for (__u32 i = 0; i < MAX_ADDRS; i++) {
-    /* Verifier doesn't like pointer to the loop var. */
-    target_key = i;
-    target     = bpf_map_lookup_elem(&exceed_addrs, &target_key);
+  /* Lookup the destination address in our address table. */
+  struct target_search_cb_ctx target = {.needle = &ipv6->daddr, .found = false};
+  bpf_for_each_map_elem(&exceed_addrs, target_search_cb, &target, 0);
 
-    /* If lookup returns nothing or found address is zero exit as that indicates
-     *we are through the list of configured addresses.
-     */
-    if (!target || !((const __u32 *)(target))[0])
-      return DEFAULT_ACTION;
-
-    if (IN6_ARE_ADDR_EQUAL(&ipv6->daddr, target))
-      /* Found the destination address in our list. Break the loop and proceed
-       * with the found target address.
-       */
-      break;
-
-    target = NULL;
-  }
-
-  if (!target)
-    return DEFAULT_ACTION;
-
+  /* If the address is found, we continue and will reply with a ICMPv6
+   * time-exceeded message if the hop limit is low enough, or reply to echo
+   * requests.
+   */
+  assert_equal(target.found, true, DEFAULT_ACTION);
   counter_increment(COUNTER_KEY_TARGET);
 
   /* Address map array is 0 indexed, but the lowest hop_limit that will reach us
@@ -240,7 +268,7 @@ int exceed2go(struct xdp_md *ctx) {
    * as the destination has already been reached so all addrs after are not
    * relevant.
    */
-  if (target_key > hop_key) {
+  if (target.key > hop_key) {
     struct in6_addr *exceed_addr = bpf_map_lookup_elem(&exceed_addrs, &hop_key);
 
     /* If there is an address found for the hop limit then reply with ICMP time
@@ -249,8 +277,8 @@ int exceed2go(struct xdp_md *ctx) {
     if (exceed_addr && ((const __u32 *)(exceed_addr))[0]) {
       counter_increment(COUNTER_KEY_FOUND);
       int ret = reply_exceeded(ctx, exceed_addr);
-      if (ret == XDP_DROP)
-        counter_increment(COUNTER_KEY_DROP);
+      if (ret == XDP_ABORTED)
+        counter_increment(COUNTER_KEY_ABORTED);
       return ret;
     }
   }
@@ -258,17 +286,12 @@ int exceed2go(struct xdp_md *ctx) {
   /* If the packet wasn't answered already with a time exceeded message, it
    * might be an ICMP echo request that should be answered for all of our addrs.
    */
-  if (ipv6->nexthdr != IPPROTO_ICMPV6)
-    return DEFAULT_ACTION;
-
+  assert_equal(ipv6->nexthdr, IPPROTO_ICMPV6, DEFAULT_ACTION);
   counter_increment(COUNTER_KEY_ICMP);
 
-  struct icmp6hdr *icmp6 = NEXT_HDR(ipv6);
-  CHECK_BOUNDARY(icmp6, data_end, DEFAULT_ACTION);
-
-  if (icmp6->icmp6_type != ICMP6_ECHO_REQUEST)
-    return DEFAULT_ACTION;
-
+  struct icmp6hdr *icmp6 = next_header(ipv6);
+  assert_boundary(icmp6, data_end, DEFAULT_ACTION);
+  assert_equal(icmp6->icmp6_type, ICMP6_ECHO_REQUEST, DEFAULT_ACTION);
   counter_increment(COUNTER_KEY_ICMP_ECHO_REQ);
 
   return reply_echo(eth, ipv6, icmp6, data_end);
