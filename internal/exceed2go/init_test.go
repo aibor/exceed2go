19e4b400e3fd8b57c213a508ed1e16ffa6817227
Tobias Böhm <code@aibor.de>
2023-06-17 13:27:56 +0200

    Refactor init helper code and wrapper script

--- internal/exceed2go/init_test.go
+++ internal/exceed2go/init_test.go
@@ -11,11 +11,7 @@ import (
 	"time"
 )
 
-func mountfs(m *testing.M, path, fstype string) error {
-	if os.Getpid() != 1 {
-		return nil
-	}
-
+func mountfs(path, fstype string) error {
 	if err := os.MkdirAll(path, 0755); err != nil {
 		return fmt.Errorf("mkdir %s: %v", path, err)
 	}
@@ -27,20 +23,7 @@ func mountfs(m *testing.M, path, fstype string) error {
 	return nil
 }
 
-func poweroff() {
-	if os.Getpid() != 1 {
-		return
-	}
-
-	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF); err != nil {
-		fmt.Printf("error calling power off: %v\n", err)
-	}
-}
-
-func run(m *testing.M) int {
-	defer poweroff()
-	fmt.Println("Init")
-
+func setupSystem() error {
 	mounts := []struct {
 		dest   string
 		fstype string
@@ -52,38 +35,68 @@ func run(m *testing.M) int {
 	}
 
 	for _, mp := range mounts {
-		if err := mountfs(m, mp.dest, mp.fstype); err != nil {
-			fmt.Println(err)
-			return 1
+		if err := mountfs(mp.dest, mp.fstype); err != nil {
+			return err
 		}
 		time.Sleep(200 * time.Millisecond)
 	}
 
-	if os.Getpid() == 1 {
-		if err := os.WriteFile("/proc/sys/kernel/printk", []byte("0"), 0755); err != nil {
-			fmt.Printf("setting printk: %v\n", err)
+	// Silence the kernel so it does not show up in our test output.
+	if err := os.WriteFile("/proc/sys/kernel/printk", []byte("0"), 0755); err != nil {
+		return fmt.Errorf("set printk: %v", err)
+	}
+
+	return nil
+}
+
+func printTrace() error {
+	f, err := os.ReadFile("/sys/kernel/tracing/trace")
+	if err != nil {
+		return fmt.Errorf("open trace pipe: %v", err)
+	}
+
+	log := make([]string, 0)
+	for _, l := range strings.Split(strings.TrimSpace(string(f)), "\n") {
+		if !strings.HasPrefix(l, "#") {
+			log = append(log, l)
+		}
+	}
+
+	if len(log) > 0 {
+		fmt.Println("Kernel trace log:")
+		fmt.Println(f)
+	}
+
+	return nil
+}
+
+func poweroff() {
+	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF); err != nil {
+		fmt.Printf("error calling power off: %v\n", err)
+	}
+}
+
+func run(m *testing.M) int {
+	isPidOne := os.Getpid() == 1
+
+	if isPidOne {
+		defer poweroff()
+
+		if err := setupSystem(); err != nil {
+			fmt.Printf("Error setting up system: %v\n", err)
 			return 1
 		}
 	}
 
-	fmt.Println("Run tests")
 	ret := m.Run()
-	if os.Getpid() != 1 {
-		return ret
-	}
-	fmt.Println("Print trace")
-	f, err := os.ReadFile("/sys/kernel/tracing/trace")
-	if err != nil {
-		fmt.Printf("error opening trace pipe: %v\n", err)
-		return ret
-	}
 
-	for _, l := range strings.Split(string(f), "\n") {
-		if !strings.HasPrefix(l, "#") {
-			fmt.Println(l)
+	if isPidOne && testing.Verbose() {
+		if err := printTrace(); err != nil {
+			fmt.Printf("Error printing trace: %v\n", err)
 		}
 	}
 
+	fmt.Printf("PIDONE_GO_TEST_RC: %d\n", ret)
 	return ret
 }
 
--- scripts/run-qemu-test.sh
+++ scripts/run-qemu-test.sh
@@ -2,34 +2,59 @@
 
 set -eEuo pipefail
 
-: ${KERNEL_FILE:=/boot/vmlinuz-linux}
+: ${RUN_QEMU_TEST_DEBUG:=}
+: ${RUN_QEMU_TEST_KERNEL:=/boot/vmlinuz-linux}
+: ${RUN_QEMU_TEST_MEMORY:=128}
+
+run_qemu() {
+	local testargs=("$@")
+	local sysargs=(
+		root=/dev/ram0
+		console=ttyAMA0
+		console=ttyS0
+		panic=-1
+		quiet
+	)
+	local cmdline="${sysargs[*]} -- ${testargs[*]}"
+
+	qemu-system-x86_64 \
+		-kernel "$RUN_QEMU_TEST_KERNEL" \
+		-initrd initrd \
+		-enable-kvm \
+		-m ${RUN_QEMU_TEST_MEMORY} \
+		-no-reboot \
+		-serial stdio \
+		-display none \
+		-append "$cmdline" \
+		</dev/null
+}
+
+if [[ -n $RUN_QEMU_TEST_DEBUG ]]; then
+	set -x
+fi
+
+testbinary="$1"
+shift
+args=("$@")
 
 # Change into the tmp dir where go built the test binary. Go test is cleaning
 # up the temp dir, so we do not need to clean up.
-pushd $(dirname $(realpath $1)) >/dev/null
-mkdir rootrd
-mv "$1" rootrd/init
-shift
-args="$*"
+pushd "$(dirname "$(realpath "$testbinary")")" >/dev/null
+# The test binary is the only thing executed in the test system so rename it
+# to "init" which is the magic name the kernel is executing.
+mv "$testbinary" init
 
-pushd rootrd >/dev/null
-find . | cpio -o -H newc | gzip -c >../rootrd.gz
-popd >/dev/null
+# Build initrd cpio archive in SRV4 format.
+echo init | cpio -o -H newc >initrd
 
-rc=0
+rc=1
 while read -r; do
-	if [[ $REPLY =~ ^FAIL || $REPLY =~ "--- FAIL:" ]]; then
-		rc=1
+	# Only our magic line is able to set rc to 0.
+	if [[ $REPLY =~ ^PIDONE_GO_TEST_RC:\ ([0-9]+) ]]; then
+		rc=${BASH_REMATCH[1]}
+	else
+		echo "$REPLY"
 	fi
-	echo "$REPLY"
-done < <(qemu-system-x86_64 \
-	-kernel "$KERNEL_FILE" \
-	-initrd rootrd.gz \
-	-enable-kvm \
-	-m 128 \
-	-serial stdio \
-	-display none \
-	-append "root=/dev/ram0 console=ttyAMA0 console=ttyS0 panic=-1 quiet -- $args" \
-	</dev/null)
+done < <(run_qemu "${args[@]}")
 
 exit "$rc"
